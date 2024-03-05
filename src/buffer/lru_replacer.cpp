f263173a686c6a8d222d5f6778ba2258e55d4471
yansong <liys0304@163.com>
2024-03-05 21:08:50 +0800

    pro.1

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -11,19 +11,55 @@
 //===----------------------------------------------------------------------===//
 
 #include "buffer/lru_replacer.h"
+#include "common/config.h"
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) { max_page_num_ = static_cast<int>(num_pages); }  //父类和子类之间转化
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+  mtx_.lock();               //访问之前上锁
+  auto vic = list_.front();  //队头元素准备出队
+  list_.pop_front();
+  map_.erase(map_.find(vic));  //出队后将哈希表中的页删除
+  if (frame_id != nullptr) {
+    *frame_id = vic;
+  }
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+  mtx_.unlock();
+  return true;
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  mtx_.lock();
+  if (map_.find(frame_id) != map_.end()) {
+    list_.erase(map_[frame_id]);
+    map_.erase(frame_id);
+  }
 
-size_t LRUReplacer::Size() { return 0; }
+  mtx_.unlock();
+}
+
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  mtx_.lock();
+  if (map_.find(frame_id) != map_.end()) {
+    mtx_.unlock();
+    return;
+  }
+  if (static_cast<int>(list_.size()) == max_page_num_) {
+    frame_id_t *vic = nullptr;
+    if (!Victim(vic)) {
+      mtx_.unlock();
+      return;
+    }
+  }
+  list_.emplace_back(frame_id);
+  map_[frame_id] = --list_.end();
+  mtx_.unlock();
+}
+
+size_t LRUReplacer::Size() { return list_.size(); }
 
 }  // namespace bustub
--- src/include/buffer/lru_replacer.h
+++ src/include/buffer/lru_replacer.h
@@ -15,6 +15,7 @@
 #include <list>
 #include <mutex>  // NOLINT
 #include <vector>
+#include <unordered_map>
 
 #include "buffer/replacer.h"
 #include "common/config.h"
@@ -47,6 +48,10 @@ class LRUReplacer : public Replacer {
 
  private:
   // TODO(student): implement me!
+  int max_page_num_;
+  std::list<frame_id_t> list_;
+  std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> map_;
+  std::mutex mtx_;
 };
 
 }  // namespace bustub
